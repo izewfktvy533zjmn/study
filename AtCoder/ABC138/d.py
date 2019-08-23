6ba61906bbc6cd3d78170f126d19e6732c6a905f
Taichi Soma <izewfktvy533zjmn@gmail.com>
2019-08-23 23:06:42 +0900

    Fix ABC138 d.py

--- AtCoder/ABC138/d.py
+++ AtCoder/ABC138/d.py
@@ -8,20 +8,23 @@ val_lst = [0 for _ in range(n)]
 for _ in range(n-1):
     a, b = [int(x) for x in input().rstrip().split()]
     branch_lst[a-1].append(b-1)
+    branch_lst[b-1].append(a-1)
+
 
 for _ in range(q):
     p, x = [int(x) for x in input().rstrip().split()]
     val_lst[p-1] += x
 
 d = deque()
-d.append(0)
+d.append((0, -1))
 
 while len(d):
-    parent = d.popleft()
+    node, parent = d.popleft()
 
-    for child in branch_lst[parent]:
-        d.append(child)
-        val_lst[child] += val_lst[parent]
+    for child in branch_lst[node]:
+        if not child == parent:
+            d.append((child, node))
+            val_lst[child] += val_lst[node]
 
 
 print(' '.join(map(str, val_lst)))
