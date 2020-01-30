558cd12a6ec858e860321dee963fb23a7faf3877
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-01-30 23:55:31 +0900

    I solved critical connections on LeetCode using lowlink algorithm

--- LeetCode/critical_connections/solution.py
+++ LeetCode/critical_connections/solution.py
@@ -1,32 +1,70 @@
 #!/usr/bin/env python3
 
-class Solution:
-    def criticalConnections(self, n, edges):
-        graph = [[] for _ in range(n)]
+def criticalConnections(n, edges):
+    graph = [[] for _ in range(n)]
+    pre = [0] * n
+    low = [0] * n
+    ans = []
 
-        for node1, node2 in edges:
-            graph[node1-1].append(node2-1)
-            graph[node2-1].append(node1-1)
-            
-        pre = [0] * n
-        low = [0] * n
-        time = 0
+    for u, v in edges:
+        graph[u-1].append(v-1)
+        graph[v-1].append(u-1)
 
-        def dfs(node, pair):
-            
 
-        return graph
+    def dfs(v, u, clock):
+        pre[v] = clock
+        low[v] = clock
+        clock += 1
+        
+        for pair in graph[v]:
+            if pre[pair] == 0:
+                clock = dfs(pair, v, clock)
+
+                low[v] = min(low[v], low[pair])
+
+                if pre[v] < low[pair]:
+                    ans.append((v+1, pair+1))
+               
+                
+            elif not pair == u:
+                low[v] = min(low[v], low[pair])
+
+
+        return clock
+
+
+    dfs(0, -1, 0)
+
+
+    return ans
+
 
 
 
 if __name__ == '__main__':
-    s = Solution()
-    result = s.criticalConnections(5, [[1, 2], [1, 3], [3, 4], [1, 4], [4, 5]])
-    print(result)
+    result = criticalConnections(5, [[1, 2], [1, 3], [3, 4], [1, 4], [4, 5]])
+    for bridge in result:
+        print(bridge)
+    print("---------------------")
 
-    result = s.criticalConnections(6, [[1, 2], [1, 3], [2, 3], [2, 4], [2, 5], [4, 6], [5, 6]])
-    print(result)
+    result = criticalConnections(6, [[1, 2], [1, 3], [2, 3], [2, 4], [2, 5], [4, 6], [5, 6]])
+    for bridge in result:
+        print(bridge)
+    print("---------------------")
 
-    result = s.criticalConnections(9, [[1, 2], [1, 3], [2, 3], [3, 4], [3, 6], [4, 5], [6, 7], [6, 9], [7, 8], [8, 9]])
-    print(result)
+    result = criticalConnections(9, [[1, 2], [1, 3], [2, 3], [3, 4], [3, 6], [4, 5], [6, 7], [6, 9], [7, 8], [8, 9]])
+    for bridge in result:
+        print(bridge)
+    print("---------------------")
+
+    result = criticalConnections(4, [[1,2], [2,3], [2,4]])
+    for bridge in result:
+        print(bridge)
+    print("---------------------")
+
+    result = criticalConnections(5, [[1,2], [2,3], [2,4], [2,5] ,[4,5]])
+    for bridge in result:
+        print(bridge)
+    print("---------------------")
+
 
