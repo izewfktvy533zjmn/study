2a053215b54c7b5c755d593ae078e9430e05b48a
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-03-08 20:17:14 +0900

    I solved question A at ABC158

---
+++ AtCoder/ABC158/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s == "AAA" || s == "BBB") cout << "No" << endl;
+    else cout << "Yes" << endl;
+             
+    return 0;
+}
+
