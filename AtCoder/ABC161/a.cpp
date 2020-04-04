aedebfcc5121fa766a0783907beb1b9bf214b0b0
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-04-04 22:14:37 +0900

    I solved question A at ABC161

---
+++ AtCoder/ABC161/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << z << " " << x << " "<< y << endl;    
+    return 0;
+}
+
